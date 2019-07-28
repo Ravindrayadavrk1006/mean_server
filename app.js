3f39ed2be409adfdb24d3b7a66479b9e61f32b5b
Ravindra Yadav <ravindrayadavrk1006@gmail.com>
2019-07-28 09:02:23 +0530

    using express-session to autorization

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session=require('express-session')
+var fileStore=require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -35,9 +38,65 @@ app.use(express.urlencoded({ extended: false }));
 //place where the autherization code is very important to first doing the auterization and then moving on to further code 
 app.use(cookieParser('12345-67890-09876-54321'));
 
-function auth (req, res, next) {
+// function auth (req, res, next) {
 
-  if (!req.signedCookies.user) {
+//   if (!req.signedCookies.user) {
+//     var authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//         return;
+//     }
+//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     var user = auth[0];
+//     var pass = auth[1];
+//     if (user == 'admin' && pass == 'password') {
+//         res.cookie('user','admin',{signed: true});
+//         next(); // authorized
+//     } else {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');              
+//         err.status = 401;
+//         next(err);
+//     }
+//   }
+//   else {
+//       if (req.signedCookies.user === 'admin') {
+//           next();
+//       }
+//       else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+// }
+
+// app.use(auth);
+
+
+
+
+
+//by using the express session
+
+
+
+
+app.use(session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store:new fileStore()
+}));
+
+
+function auth (req, res, next) {
+  console.log(req.session);
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -50,7 +109,7 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
+        req.session.user='admin'
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
@@ -60,7 +119,7 @@ function auth (req, res, next) {
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           next();
       }
       else {
@@ -72,6 +131,10 @@ function auth (req, res, next) {
 }
 
 app.use(auth);
+
+
+
+
 
 
 
